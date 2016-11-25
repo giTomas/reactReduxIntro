e09ae4b36a831c79f6c47e22cf154160b8bb1c56
Kosegi <SK040122@main.intgin.net>
2016-11-25 12:27:16 +0100

    finalized

--- Auth.jsx
+++ Auth.jsx
@@ -1,30 +1,46 @@
 import React from 'react';
 import {createStore} from 'redux';
 
+
+//actions
+function login(username) {
+  return {
+    type: 'LOGIN',
+    value: username
+  }
+}
+
+function logout() {
+    return  {
+      type: 'LOGOUT',
+      value: 'guest'
+    };
+}
+
 //reducer
 const auth = function(state = {status: 'logged out', value: 'guest'}, action) {
   switch (action.type) {
     case 'LOGIN':
-      return Object.assign({
+      /*return Object.assign({}, state, {
         status: 'logged in',
         value: action.value
-      })
+      })*/
+      return {...state, status: 'logged in', value: action.value}
     case 'LOGOUT':
-      return Object.assign({}, state, {
+      /*return Object.assign({}, state, {
         status: 'logged out',
         value: action.value
-      })
+      })*/
+      return {...state, status: 'logged out', value: action.value}
     default:
       return state;
   }
-};
+}
 
 //store
-
-const store = createStore(auth);
+export const store = createStore(auth);
 
 //react component
-
 class Auth extends React.Component{
   constructor(props) {
     super(props);
@@ -34,48 +50,63 @@ class Auth extends React.Component{
   }
 
   handleLogin() {
-    let username = this.refs.username.values;
+    let {value: username} = this.refs.username;
     // dispatch action
-    store.dispatch({
+    /*store.dispatch({
       type: 'LOGIN',
       value: username
-    });
+    });*/
+    store.dispatch(login(username));
   }
   handleLogout() {
-
+    //dispatch action
+    /*store.dispatch({
+      type: 'LOGOUT',
+      value: 'guest'
+    })*/
+    store.dispatch(logout());
+    this.refs.username.value = '';
   }
 
   render() {
+    const {status, value} = this.props.state;
+    let button = status === 'logged in' ?
+                 <button onClick={this.handleLogout}>Log out</button> :
+                 <input type='button' value='Login' onClick={this.handleLogin}/>;
+
     return (
       <div>
         <input type='text' ref='username'/>
-        <input type='button' value='Login' onClick={this.handleLogin}/>
-        <h1>Current state is {this.props.state.status +  ' as ' +
-          this.props.state.value
-        }</h1>
+        {button}
+        {/* <input type='button' value='Login' onClick={this.handleLogin}/> */}
+        <h1>Current state is {`${status} as ${value}`}</h1>
       </div>
     );
   }
 }
 
 
+
+// react component
 /*
-const Auth = React.createClass({
+var Auth = React.createClass({
   handleLogin: function() {
     let username = this.refs.username.value;
-    //dispatch action
+    // dispatch action
     store.dispatch({
-      type: 'LOGIN',
+        type: 'LOGIN',
       value: username
     });
   },
-  handleLogout: function() {},
+  handleLogout: function() {
+
+  },
   render: function() {
     return (
       <div>
-        <input type='text' ref='username'/>
-        <input type='button' value='login' onClick={this.handleLogin}/>
-        <h1>Current state is ...</h1>
+       <input type="text" ref="username" />
+       <input type="button" value="Login" onClick={this.handleLogin} />
+       <h1>Current state is {this.props.state.status + ' as ' + this.props.state.value}</h1>
       </div>
     );
   }
--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import {createStore} from 'redux';
 import Auth from './Auth.jsx';
+// import {auth} from './Auth.jsx';
+import {store} from './Auth.jsx';
 
-ReactDOM.render(<Auth />,
-  document.getElementById('root')
-);
+console.log(store.getState());
+
+const render = function() {
+
+  ReactDOM.render(
+    <Auth state={store.getState()}/>,
+    document.getElementById('root')
+  );
+
+};
+
+
+store.subscribe(render);
+render();
